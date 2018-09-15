e2f95b2d8e0b402e83bdb192b64b8d95593ea208
Fernando Morantes <43284808+FernandoMorantes@users.noreply.github.com>
2018-09-15 13:54:38 -0500

    Add files via upload

--- main.rb
+++ main.rb
@@ -5,15 +5,20 @@ begin
     host = String('postgres-hackathon.eastus2.cloudapp.azure.com')
     database = String('storekeepersdb')
     user = String('hackathonpostgres')
-    password = String('<hackathon2018rappisql>')
+    password = String('hackathon2018rappipsql')
 
     # Initialize connection object.
     connection = PG::Connection.new(:host => host, :user => user, :dbname => database, :port => '5432', :password => password)
     puts 'Successfully created connection to database.'
 
-    
+    resultSet = connection.exec('SELECT * from storekeepers;')
+    resultSet.each_with_index do |row, i|
+        puts "Data row #{i} :\n id: #{ row['id']}\n storekeeper id: #{row['storekeeper_id']}\n latitud: #{row['lat']}\n longitud: #{row['lng']}\n timestamp: #{row['timestamp']}\n toolkit: #{row['toolkit']}\n" if i < 1
+    end
+
+
 rescue PG::Error => e
-    puts e.message
+    puts "#{e.message} no funciona esta puta mierda"
 
 ensure
     connection.close if connection
